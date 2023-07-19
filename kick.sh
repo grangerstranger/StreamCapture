79b95892463acd06bbd3f3f59f191a779028b65e
x-it-tech <70045256+x-it-tech@users.noreply.github.com>
2023-07-18 20:42:26 -0700

    Updates to Dest Path

--- kick.sh
+++ kick.sh
@@ -7,5 +7,5 @@ fnKickStart(){
 	done
 
 if [[ -z $(ps -ef | grep -v grep | grep "https://kick.com/$streamer" | grep streamlink) ]]; then
-	screen -dmS $streamer bash -c "streamlink --output \"$destpath/{author}/{title} {id}.ts\" https://kick.com/$streamer best"
+	screen -dmS $streamer bash -c "streamlink --output \"$destpath/kick/{author}/{title} {id}.ts\" https://kick.com/$streamer best"
 fi
--- streamcapture.sh
+++ streamcapture.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 twitchstreamers=(crazymango_vr aeriytheneko isthisrealvr)
+kickstreamer=crazymangovr
 game="VRChat"
 destpath="./" #Change to appropriate location. Will save to local if not defined
 configfile="./.twitchrecord.conf"
 authorizationfile="./.twitchcreds.conf"
 
-kickstreamer=crazymangovr
-kickdestpath="/Nyx"
+
 
 source ./twitch.sh
 source ./kick.sh
